cc13cca13368b16199ac389deb27a4cb43e2a2f1
Joy Kyser <joy.kyser@students.pcci.edu>
2023-08-18 17:05:22 -0400

    Current Website Code updated locally

--- footer.php
+++ footer.php
@@ -1,52 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-        <link rel="icon" href="Icons/letter-j-icon-png-21789-Windows.ico">
-        <title>About | Contact - Joy Kyser</title>
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="nofollow" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-        <!-- Stylesheets -->
-        <link rel="stylesheet" href="site.css">
-
-        <!-- JQuery and JS -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-        <script src="site.js"></script>
-
-        <!-- Imported font for the site -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lusitana&family=Roboto+Mono&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Palanquin:wght@200&display=swap" rel="stylesheet">
-    </head>
-    <div id="#footer" class="floating-footer">
-        <div class="footer container">
-            <div class="icon-list text-center">
-                        <a href="https://www.facebook.com/joy.kyser" target="_blank"><img class="icons text-center" src="Icons/facebook.png" target="_blank" alt="Facebook Link Icon"/></a>
-                        <a href="#"><img class="icons text-center" src="Icons/instagram.png" target="_blank" alt="Instagram Link Icon"/></a>
-                        <a href="https://www.linkedin.com/in/joy-kyser-135092175/" target="_blank" alt="LinkedIn Link Icon"><img class="icons text-center" src="Icons/linkedin.png" /></a>                        
-            </div><!--
-         --><div class="portfolio-attributions">
-                <p class="portfolio">Portfolio - Joy Kyser <script>document.write( new Date().getFullYear() );</script></p>
-                <button type="button" class="info-icon" data-toggle="tooltip" data-placement="top" data-html="true" data-container="body"
-                    title="<h3>Social Media Icons:</h3><a href='https://www.freepik.com/free-vector/social-media-icons-vector-set-with-facebook-instagram-twitter-tiktok-youtube-logos_17221200.htm#query=social%20media%20icon&position=0&from_view=keyword&track=ais' class='tooltip-link'>Image by rawpixel.com</a>
+<!doctypehtml><html lang="en"><head><meta charset="utf-8"><meta content="width=device-width,initial-scale=1,shrink-to-fit=no"name="viewport"><meta content="Joy Kyser"property="og:title"><meta content="https://i.ibb.co/qxvJ1GW/Profile-Picture-BW.png"property="og:image"><meta content="Portfolio"property="og:description"><meta content="https://joykyser.com/"property="og:url"><meta content="1200"property="og:image:width"><meta content="627"property="og:image:height"><meta content="website"property="og:type"><link rel="icon"href="Icons/letter-j-icon-png-21789-Windows.ico"><title>About | Contact - Joy Kyser</title><link rel="stylesheet"rel="nofollow"href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"crossorigin="anonymous"integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"><link rel="stylesheet"href="site.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script><script src="site.js"></script><link rel="preconnect"href="https://fonts.googleapis.com"><link rel="preconnect"href="https://fonts.gstatic.com"crossorigin><link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Lusitana&family=Roboto+Mono&display=swap"><link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"><link rel="stylesheet"href="https://fonts.googleapis.com/css2?family=Palanquin:wght@200&display=swap"></head><div class="floating-footer"id="#footer"><div class="container footer"><div class="text-center icon-list"><a href="https://www.facebook.com/joy.kyser"target="_blank"><img src="Icons/facebook.png"alt="Facebook Link Icon"class="text-center icons"target="_blank"></a><a href="#"><img src="Icons/instagram.png"alt="Instagram Link Icon"class="text-center icons"target="_blank"></a><a href="https://www.linkedin.com/in/joy-kyser-135092175/"target="_blank"alt="LinkedIn Link Icon"><img src="Icons/linkedin.png"class="text-center icons"></a></div><div class="portfolio-attributions"><p class="portfolio">Portfolio - Joy Kyser<script>document.write((new Date).getFullYear())</script></p><button class="info-icon"data-container="body"data-html="true"data-placement="top"data-toggle="tooltip"title="<h3>Social Media Icons:</h3><a href='https://www.freepik.com/free-vector/social-media-icons-vector-set-with-facebook-instagram-twitter-tiktok-youtube-logos_17221200.htm#query=social%20media%20icon&position=0&from_view=keyword&track=ais' class='tooltip-link'>Image by rawpixel.com</a>
                             <h3>Coding Image:</h3><a href='https://www.vecteezy.com/free-photos' class='tooltip-link'>Free Stock photos by Vecteezy</a>
                             <h3>Music Recording Image:</h3><a href='https://www.pexels.com/photo/back-view-of-a-woman-recording-a-song-in-a-music-studio-7086742/' class='tooltip-link'>Photo by cottonbro studio</a>
                             <h3>Play Button:</h3><a href='https://www.flaticon.com/free-icons/pause' title='pause icons' class='tooltip-link'>Pause icons created by IYAHICON - Flaticon</a>
                             <h3>Pause Button:</h3><a href='https://www.flaticon.com/free-icons/pause' title='pause icons' class='tooltip-link'>Pause icons created by Kiranshastry - Flaticon</a>
                             <h3>Scroll Buttons:</h3><a href='https://www.flaticon.com/free-icons/fast-forward' title='fast forward icons' class='tooltip-link'>Fast forward icons created by Smashicons - Flaticon</a>
                             <h3>Info Icon:</h3><a href='https://www.flaticon.com/free-icons/information' title='information icons' class='tooltip-link'>Information icons created by Freepik - Flaticon</a>
-                            <h3>Menu Icon:</h3><a target='_blank' class='attr-link' href='https://icons8.com/icon/3095/menu'>Menu</a><span class='attr-text'> icon by </span><a target='_blank' class='attr-link' href='https://icons8.com'>Icons8</a>"                             
-                    >
-                    <img src="Icons/info-icon.png" alt="Attribution Links">
-                </button>
-            </div>
-        </div>
-    </div>
-</html>
+                            <h3>Menu Icon:</h3><a target='_blank' class='attr-link' href='https://icons8.com/icon/3095/menu'>Menu</a><span class='attr-text'> icon by </span><a target='_blank' class='attr-link' href='https://icons8.com'>Icons8</a>"type="button"><img src="Icons/info-icon.png"alt="Attribution Links"></button></div></div></div></html>
--- site.js
+++ site.js
@@ -1,21 +1,7 @@
 // All JQuery Code
-$(document).ready(
-
-    // Highlights the page the user is on in the navbar
-    function(){
-        $("[href]").each(function() {
-            if (this.href == window.location.href) {
-                $(this).addClass("current");
-            }
-        });
-    },
-      
-    // Enable tooltips for attributions info
-    $(function () {
-        $("body").tooltip({ selector: '[data-toggle=tooltip]' });   
-    }),
-
-    // Event listeners for enabling submit button
+$(document).ready(// Highlights the page the user is on in the navbar
+function(){ $("[href]").each(function() { if (this.href == window.location.href) { $(this).addClass("current"); } }); },// Enable tooltips for attributions info
+$(function () { $("body").tooltip({ selector: '[data-toggle=tooltip]' }); }),// Event listeners for enabling submit button
     $(function() {
         var inputs = document.getElementsByClassName('form-control');
 
@@ -24,25 +10,6 @@ $(document).ready(
         }
         
     }),
-
-    // Popup nav menu for mobile devices
-    $('#link-wrapper').on('click', function() {
-        $('#mobile-links').toggle();
-
-
-
-        if ($('#mobile-links').css('display') === 'none' ) {
-            $('#link-wrapper').css('background-color', 'transparent');
-            $('#link-wrapper').css({
-                'border-left': 'none',
-                'border-bottom': 'none'});
-        }
-        else {
-            $('#link-wrapper').css('background-color', 'white');
-            $('#link-wrapper').css({
-                'border-left': '1px solid lightgray',
-                'border-bottom': '1px solid lightgray'});
-        }
-    })
-);
+// Popup nav menu for mobile devices
+$('#link-wrapper').on('click', function() { $('#mobile-links').toggle();if ($('#mobile-links').css('display') === 'none' ) { $('#link-wrapper').css('background-color', 'transparent'); $('#link-wrapper').css({ 'border-left': 'none', 'border-bottom': 'none'}); } else { $('#link-wrapper').css('background-color', 'white'); $('#link-wrapper').css({ 'border-left': '1px solid lightgray', 'border-bottom': '1px solid lightgray'}); } }) );
 
